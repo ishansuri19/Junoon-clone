815fdaa76524160283286996029d63ec1099c62b
Ishan Suri <ishansuri@Ishans-MacBook-Air.local>
2022-05-17 21:37:54 +0530

    first

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+
+function footer() {
+  return (
+    <div className="footer">
+        {/* <h1 className="tittle" > Junoon</h1> */}
+        <div className="footerlogo">
+        <img className="footerlogo" src="/images/junoon1.png" />
+        </div>
+    </div>
+  )
+}
+
+export default footer
